ab8a3c65bd5a5a0aee3caa7552e0f1fc2471bcec
markosnop <markos.vpd@gmail.com>
2023-08-08 21:51:45 -0300

    .

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 function cadastrar() {
-    const nome=document.getElementById('nome').value 
-    const idade=parseInt(document.getElementById('idade').value )
-    const email=document.getElementById('email').value 
-
+    const nome=document.getElementById("nome").value 
+    const idade=parseInt(document.getElementById("idade").value )
+    const email=document.getElementById("emai").value 
 
 const pessoa={
     nome: nome,
     idade: idade,
     email: email,
 };
-
-
     document.getElementById('result').innerHTML=pessoa;
 }
